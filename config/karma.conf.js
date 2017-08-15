e5b00903ee87800d8d0af0931ca1bad801696fe6
Rajesh <Ra@gmail.com>
2017-08-15 20:22:29 +0530

    modified karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -32,15 +32,15 @@ module.exports = function (config) {
       'dist/vendor/**/*.spec.js'
     ],
     preprocessors: {},
-    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false
-	 reporters = ['dots', 'junit'];
+	 reporters = ['progress','dots', 'junit'];
      junitReporter = {
+	 outputDir: 'karma-results',	 
      outputFile: 'test-results.xml'
      };
   });
